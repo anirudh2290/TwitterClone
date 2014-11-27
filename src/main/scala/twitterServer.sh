d3db3ff22d434cf8631606c08d0c6de0a8e62e82
Anirudh Subramanian <anirudh2290@gmail.com>
2014-11-26 19:52:44 -0500

    Add scripts to run multiple times

---
+++ src/main/scala/twitterServer.sh
@@ -0,0 +1,18 @@
+numUsers=(5000 7000 10000 30000 50000 80000 100000 120000 150000)
+numberOfLBs=(2)
+isServer=true
+#for variable lbs
+#echo "\"runMain project4 ${numUsers[$i]} true ${numberOfLBs[$j]}\""
+i=0	
+while [ $i -lt ${#numUsers[@]} ]
+do
+	j=0
+	while [ $j -lt ${#numberOfLBs[@]} ]
+	do
+		# numUsers isserver numberofLBs
+		#echo ${numUsers[$i]} ${isServer} ${numberOfLBs[$j]}
+		sbt "\"runMain project4 ${numUsers[$i]} true ${numberOfLBs[$j]}\"" >> serverfile${numUsers[$i]}
+		j=$((j+1))
+	done
+	i=$((i+1))
+done
